d4a4ca04f37fb3fbc27614a3ef7d97885e9932c9
Muralit20 <muralikannat20@gmail.com>
2023-08-27 09:53:24 +0530

    v1.0.0

---
+++ terraform-backend-state.tf
@@ -0,0 +1,27 @@
+resource "aws_s3_bucket" "bucket" {
+    bucket = "muvi-terraform-state-backend"
+    versioning {
+        enabled = false
+    }
+    server_side_encryption_configuration {
+        rule {
+            apply_server_side_encryption_by_default {
+                sse_algorithm = "AES256"
+            }
+        }
+    }
+    object_lock_configuration {
+        object_lock_enabled = "Enabled"
+    }
+}
+
+resource "aws_dynamodb_table" "terraform-lock" {
+    name           = "muvi-state-table"
+    read_capacity  = 5
+    write_capacity = 5
+    hash_key       = "LockID"
+    attribute {
+        name = "LockID"
+        type = "S"
+    }
+}
